66aceecf6da2e53c1e037440f195cd08c09a5367
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-10-15 19:43:36 -0600

    feat: add relevent h1 to each html file, use the request object path to determine what page was requested, then load the corresponding html file

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+const port = process.env.PORT || 8080;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
 
-    const port = process.env.PORT || 8080;
-    const data =fs.readFileSync("index.html")
-    
+  switch (req.url) {
+    case "/":
+      res.end(fs.readFileSync("index.html"));
+      break;
+    case "/about":
+      res.end(fs.readFileSync("about.html"));
+      break;
+    case "/contact-me":
+      res.end(fs.readFileSync("contact-me.html"));
+      break;
+    default:
+      res.end(fs.readFileSync("404.html"));
+      break;
+  }
+});
 
-    const server = http.createServer((req, res) =>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html')
-    res.end(`${url} AND ${method}`)
-    })
-    
-server.listen(port, () =>{
-    console.log('Server running')
-})
-
+server.listen(port, () => {
+  console.log("Server running");
+});
